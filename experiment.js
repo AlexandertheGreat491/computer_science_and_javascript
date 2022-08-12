e67607d3887cca471d2b8b073f27a2b327f9aead
agvan <agvandyke25@gmail.com>
2022-08-12 14:36:09 -0500

    continued experimenting with JavaScript

--- experiment.js
+++ experiment.js
@@ -12,7 +12,7 @@ buttonEl.addEventListener("click", clickHandler);*/
 
 //code example using a closure
 
-const buttonEl = document.getElementById('my-button');
+/*const buttonEl = document.getElementById('my-button');
 
 const clickHandler = function() {
   let count = 0;
@@ -23,4 +23,24 @@ const clickHandler = function() {
   };
 };
 
-buttonEl.addEventListener('click', clickHandler());
+buttonEl.addEventListener('click', clickHandler());*/
+
+//another way of using closures
+/*const buttons = document.getElementsByTagName('button');
+
+for (let i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', clickHandler());
+}*/
+
+//attaches the same self-contained closure to every <button> element
+
+const containerEl = document.getElementById('container');
+
+const clickHandler = function(event) {
+    if (event.target.matches('button')) {
+        event.target.textContent = 'Clicked!';
+    }
+};
+
+containerEl.addEventListener('click', clickHandler);
+

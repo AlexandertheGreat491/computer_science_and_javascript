886cabb8cba8fd91ed5cee34b3e3fb168c1e091e
agvan <agvandyke25@gmail.com>
2022-08-14 12:16:42 -0500

    found bubble sort's best case scenario because if array is already sorted, bubble sort only needs to traverse it once

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const { bubbleSort } = require("./sort");
 const {quickSort} = require('./sort');
 
 const numbers = [];
-for (let i = 0; i < 10000; i++) {
-  numbers.push(Math.floor(Math.random() * 10000) + 1);
+for (let i = 0; i < 5000; i++) {
+  numbers.push(i);
 }
 
 const suite = new Benchmark.Suite();

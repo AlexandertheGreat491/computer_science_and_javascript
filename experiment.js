edd5ad1dec9724d4795672637515906f47293b6a
agvan <agvandyke25@gmail.com>
2022-08-15 12:36:42 -0500

    added Fibonacci sequence pseudocode

--- experiment.js
+++ experiment.js
@@ -278,6 +278,32 @@ then the indexes will be swapped.*/
 
 //console.log(data);
 
+//Fibonacci sequence pseudocode
 
+//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, etc ...
+
+//if num <= 2
+//return 1 (the first two numbers in sequence)
+
+//otherwise return
+//same logic with num - 1
+//plus
+//same logic with num - 2
+
+//adds the number 1 over and over again until the desired result is reached
+
+//another solution
+
+// if num <= 2
+// return 1 (the first two numbers in sequence)
+
+// create new array with [1, 1]
+
+// loop from i = 2 to num
+// push [i - 1] + [i - 2] to array
+
+// return last item in array
+
+//main difference is that the numbers are put into an array in the second example
 
 
